b37728da57e014620a888d765dc3ea7db5cc1b16
THIAM Cheikh <cthiam@fluxod.com>
2022-03-04 13:14:55 +0100

    "Test from vscode edit "

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log("ceci est un test")
   await app.listen(3000);
 }
 bootstrap();
