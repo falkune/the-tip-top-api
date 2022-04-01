9d10d8486d9ee28c5f6e5a43bded8a172adad29c
Donald D'aquin Komguem Fotso <donalddev99@gmail.com>
2022-04-01 08:51:33 +0200

    Test command on remote

--- infra/scripts/deploy.sh
+++ infra/scripts/deploy.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-
-[ -d ~/.ssh ] || mkdir ~/.ssh && chmod 0700 ~/.ssh
-ssh-keyscan -t rsa -H 45.155.170.65 >> ~/.ssh/known_hosts
-ssh -o StrictHostKeyChecking=no -l donald 45.155.170.65 ls
+ls
