5d8b82ec903b0cf2344f6d926c77372019ddf9a9
Donald D'aquin Komguem Fotso <donalddev99@gmail.com>
2022-04-01 12:24:00 +0200

    Update the deploy script

---
+++ infra/scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ envr="$1"
+
+ if [ -d ~/.ssh ]; then
+     echo "already exists"
+ else
+   mkdir ~/.ssh && chmod 0700 ~/.ssh 
+   ssh-keyscan -t rsa -H 45.155.170.65 >> ~/.ssh/known_hosts
+ fi    
+  
+ scp -r infra/env/"${envr}" donald@45.155.170.65:~/app/api
+                              
+ ssh -o StrictHostKeyChecking=no -l donald 45.155.170.65 'chmod +x ./app/api/"${envr}"/deploy.sh'
+                               
+ ssh -o StrictHostKeyChecking=no -l donald 45.155.170.65 './app/api/"${envr}"/deploy.sh' 
