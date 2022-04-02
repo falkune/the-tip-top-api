9358a90f3a04343c0a283113e5fd334d01d6e43a
Donald D'aquin Komguem Fotso <donalddev99@gmail.com>
2022-04-02 18:38:19 +0200

    Test the workflow

--- infra/scripts/registry.sh
+++ infra/scripts/registry.sh
@@ -5,31 +5,24 @@ echo "push images to our private registry hub"
 #The current version  
 tag=${IMAGE_TAG}
 
+#The current image (can be db or api)
+image=$1
+
 if [ "$BRANCH_NAME" = "dev" ]; then
    
-   db=${DOCKER_PRIVATE_REGISTER}/"release/db":${tag}
-   db_latest=${DOCKER_PRIVATE_REGISTER}/"release/db:latest"
-
-   api=${DOCKER_PRIVATE_REGISTER}/"release/api":${tag}
-   api_latest=${DOCKER_PRIVATE_REGISTER}/"release/api:latest"
-   
-   docker image push --all-tags "${DOCKER_PRIVATE_REGISTER}"/"release/db"
-   docker image push --all-tags "${DOCKER_PRIVATE_REGISTER}"/"release/api"
-
-   docker rmi "${db}" "${db_latest}" "${api}" "${api_latest}"
+   image=${DOCKER_PRIVATE_REGISTER}/"release/"${image}:${tag}
+   image_latest=${DOCKER_PRIVATE_REGISTER}/"release/${image}:latest"
+  
+   docker image push --all-tags "${DOCKER_PRIVATE_REGISTER}"/"release/${image}"
+   docker rmi "${image}" "${image_latest}"
 
 
 else
    
-   db=${DOCKER_PRIVATE_REGISTER}/"stable/db":${tag}
-   db_latest=${DOCKER_PRIVATE_REGISTER}/"stable/db:latest"
-
-   api=${DOCKER_PRIVATE_REGISTER}/"stable/api":${tag}
-   api_latest=${DOCKER_PRIVATE_REGISTER}/"stable/api:latest"
-   
-   docker image push --all-tags "${DOCKER_PRIVATE_REGISTER}"/"stable/db"
-   docker image push --all-tags "${DOCKER_PRIVATE_REGISTER}"/"stable/api"
-
-   docker rmi "${db}" "${db_latest}" "${api}" "${api_latest}"
+   image=${DOCKER_PRIVATE_REGISTER}/"stable/"${image}:${tag}
+   image_latest=${DOCKER_PRIVATE_REGISTER}/"stable/${image}:latest"
+  
+   docker image push --all-tags "${DOCKER_PRIVATE_REGISTER}"/"release/${image}"
+   docker rmi "${image}" "${image_latest}"
 
 fi
