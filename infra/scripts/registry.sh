1ef934ca66350eeb9c3b2d9f9b67e395e709c68e
Donald D'aquin Komguem Fotso <donalddev99@gmail.com>
2022-04-02 18:56:05 +0200

    Test the workflow

--- infra/scripts/registry.sh
+++ infra/scripts/registry.sh
@@ -10,19 +10,16 @@ image=$1
 
 if [ "$BRANCH_NAME" = "dev" ]; then
    
-   image=${DOCKER_PRIVATE_REGISTER}"/release/"${image}:${tag}
-   image_latest=${DOCKER_PRIVATE_REGISTER}"/release/${image}:latest"
+   formated="${DOCKER_PRIVATE_REGISTER}/release/${image}:${tag}"
+   formated_latest="${DOCKER_PRIVATE_REGISTER}/release/${image}:latest"
   
-   docker image push --all-tags "${DOCKER_PRIVATE_REGISTER}/release/${image}"
-   docker rmi "${image}" "${image_latest}"
-
-
 else
    
-   image=${DOCKER_PRIVATE_REGISTER}/"stable/"${image}:${tag}
-   image_latest=${DOCKER_PRIVATE_REGISTER}/"stable/${image}:latest"
+   formated=${DOCKER_PRIVATE_REGISTER}/"stable/"${image}:${tag}
+   formated_latest=${DOCKER_PRIVATE_REGISTER}/"stable/${image}:latest"
   
-   docker image push --all-tags "${DOCKER_PRIVATE_REGISTER}/stable/${image}"
-   docker rmi "${image}" "${image_latest}"
-
 fi
+
+docker push "${formated}"
+docker push "${formated_latest}"
+docker rmi "${formated}" "${formated_latest}"
