0e1221239055d0662d20d38a3a4ce50917554e89
Donald D'aquin Komguem Fotso <donalddev99@gmail.com>
2022-04-02 19:21:34 +0200

    Update the api Dockerfile, change the entrypoints

--- infra/env/release/start.sh
+++ infra/env/release/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd "app/api/release" || exit
 docker compose down
-docker compose ps 
-#docker compose up -d 
+docker rmi registry.dsp-archiwebo21-ct-df-an-cd.fr/release/api
+docker rmi registry.dsp-archiwebo21-ct-df-an-cd.fr/release/db
+docker compose up -d  
