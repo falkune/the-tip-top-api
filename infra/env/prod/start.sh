444fc14835d1a03f313940e3a3a429722513065a
Donald D'aquin Komguem Fotso <donalddev99@gmail.com>
2022-04-04 01:00:05 +0200

    fix env variable in prod

--- infra/env/prod/start.sh
+++ infra/env/prod/start.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-ls
+cd "app/api/prod" || exit
+docker compose down
+docker rmi registry.dsp-archiwebo21-ct-df-an-cd.fr/stable/api
+docker rmi registry.dsp-archiwebo21-ct-df-an-cd.fr/stable/db
+docker compose up --build -d  
