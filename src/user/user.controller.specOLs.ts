164958f862834d4c5439c173b31b8a90a08e68b9
Cheikh THIAM <cheikhthiam95g@gmail.com>
2022-11-18 13:11:44 +0100

    Addinf controller.spec.ts for user entity

---
+++ src/user/user.controller.specOLs.ts
@@ -0,0 +1,26 @@
+/*import { Test, TestingModule } from '@nestjs/testing';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { MailerService } from '@nestjs-modules/mailer'; 
+import { UserModule } from './user.module';
+import { MailModule } from '../mail/mail.module';
+import { MailController } from '../mail/mail.controller';
+
+describe('UserController', () => {
+  let userController: UserController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [UserController,MailController],
+      providers: [UserService,MailerService],
+      imports: [UserModule,MailModule]
+    }).compile();
+
+    userController = app.get<UserController>(UserController);
+  });
+  it('it should  be defined', () => {
+    expect(userController).toBeDefined();
+  });
+
+
+});    */
