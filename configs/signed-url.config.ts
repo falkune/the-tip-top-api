842dd511d1f1b73cb605115195e6f50d6ab3af62
Cheikh THIAM <cthiam@fluxod.com>
2022-12-09 08:36:35 +0100

    update app.module frome release

--- configs/signed-url.config.ts
+++ configs/signed-url.config.ts
@@ -2,7 +2,7 @@ import { UrlGeneratorModuleOptions } from 'nestjs-url-generator';
 
 export function urlGeneratorModuleConfig(): UrlGeneratorModuleOptions {
   return {
-    secret: "66C5C66A5B3E51AF919E8BD7ED4AC1C4CB28A7455F9D269E1A5CAB7C5B", // optional, required only for signed URL
-    appUrl: "https://api.dev.dsp-archiwebo21-ct-df-an-cd.fr"
+    secret: process.env.APP_KEY,
+    appUrl: process.env.APP_URL,
   };
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -12,8 +12,8 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule } from '@nestjs/config'; 
 import { FacebookStrategy } from './auth/strategies/facebook.strategy';
 import { GoogleStrategy } from './auth/strategies/google.strategy';
-import { UrlGeneratorModule } from 'nestjs-url-generator'; 
-import { urlGeneratorModuleConfig } from 'configs/signed-url.config';
+import { UrlGeneratorModule } from 'nestjs-url-generator';
+//import { urlGeneratorModuleConfig } from 'configs/signed-url.config';
 
 
 @Module({
@@ -21,12 +21,9 @@ import { urlGeneratorModuleConfig } from 'configs/signed-url.config';
 //MongooseModule.forRoot("mongodb://localhost:27017/thetiptop"),
  MongooseModule.forRoot(process.env.MONGO_URI),
  
-  // UrlGeneratorModule.forRoot({
-  //   secret: "66C5C66A5B3E51AF919E8BD7ED4AC1C4CB28A7455F9D269E1A5CAB7C5B", // optional, required only for signed URL
-  //   appUrl: "https://api.dev.dsp-archiwebo21-ct-df-an-cd.fr"
-  // }),
-  UrlGeneratorModule.forRootAsync({
-    useFactory: () => urlGeneratorModuleConfig(),
+  UrlGeneratorModule.forRoot({
+    secret: process.env.APP_KEY, // optional, required only for signed URL
+    appUrl: process.env.APP_URL
   }),
     MailerModule.forRoot({
       transport: {
@@ -56,4 +53,3 @@ export class AppModule {}
  
 
 
- 
