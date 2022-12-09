4ac9a2186550776fd89913f33376e24ad9c18966
Cheikh THIAM <cthiam@fluxod.com>
2022-12-09 07:45:32 +0100

    Change the way we create UrlGeneratorModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import { ConfigModule } from '@nestjs/config';
 import { FacebookStrategy } from './auth/strategies/facebook.strategy';
 import { GoogleStrategy } from './auth/strategies/google.strategy';
 import { UrlGeneratorModule } from 'nestjs-url-generator'; 
+import { urlGeneratorModuleConfig } from 'configs/signed-url.config';
 
 
 @Module({
@@ -20,9 +21,12 @@ import { UrlGeneratorModule } from 'nestjs-url-generator';
 //MongooseModule.forRoot("mongodb://localhost:27017/thetiptop"),
  MongooseModule.forRoot(process.env.MONGO_URI),
  
-  UrlGeneratorModule.forRoot({
-    secret: "66C5C66A5B3E51AF919E8BD7ED4AC1C4CB28A7455F9D269E1A5CAB7C5B", // optional, required only for signed URL
-    appUrl: "https://api.dev.dsp-archiwebo21-ct-df-an-cd.fr"
+  // UrlGeneratorModule.forRoot({
+  //   secret: "66C5C66A5B3E51AF919E8BD7ED4AC1C4CB28A7455F9D269E1A5CAB7C5B", // optional, required only for signed URL
+  //   appUrl: "https://api.dev.dsp-archiwebo21-ct-df-an-cd.fr"
+  // }),
+  UrlGeneratorModule.forRootAsync({
+    useFactory: () => urlGeneratorModuleConfig(),
   }),
     MailerModule.forRoot({
       transport: {
