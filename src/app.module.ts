425063b472ed43727e5842b9e3bea44b92e7b9fe
Cheikh THIAM <cthiam@fluxod.com>
2022-12-09 07:30:57 +0100

    load app_url and secret from app.module.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,8 +21,8 @@ import { UrlGeneratorModule } from 'nestjs-url-generator';
  MongooseModule.forRoot(process.env.MONGO_URI),
  
   UrlGeneratorModule.forRoot({
-    secret: process.env.APP_KEY, // optional, required only for signed URL
-    appUrl: process.env.APP_URL
+    secret: "66C5C66A5B3E51AF919E8BD7ED4AC1C4CB28A7455F9D269E1A5CAB7C5B", // optional, required only for signed URL
+    appUrl: "https://api.dev.dsp-archiwebo21-ct-df-an-cd.fr"
   }),
     MailerModule.forRoot({
       transport: {
